72275150bb8664bc6e18ff71d3051ee3d356562f
biupripro <biupripro@163.com>
2023-04-05 22:18:01 +0800

    实现下载Google的Logo图到本地文件

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import okhttp3.*
+import java.io.File
 import java.net.InetSocketAddress
 import java.net.Proxy
 import java.net.ProxySelector
@@ -15,12 +16,23 @@ fun main(args: Array<String>) {
     println("http client on socks proxy created")
 
     val request = Request.Builder().apply {
-        url("https://i.pximg.net/img-original/img/2023/04/05/17/00/06/106888963_p0.png")
+        url("https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png")
+//        addHeader("Referrer-Policy", "strict-origin-when-cross-origin")
+//        addHeader(":authority", "i.pximg.net")
+//        addHeader(":method", "GET")
+//        addHeader(":path", "/img-original/img/2023/04/05/17/00/06/106888963_p0.png")
+//        addHeader(":scheme", "https")
     }.build()
-    println("http request created")
+    println("http request created [${request.headers}]")
 
     val resp = client.newCall(request).execute()
     println("http response got, code is [${resp.code}]")
+
+    val bytes = resp.body?.bytes() ?: ByteArray(0)
+    File("D:/tmp/zzz.png").apply {
+        parentFile.mkdirs()
+        writeBytes(bytes)
+    }
 
     val n = 0
 
