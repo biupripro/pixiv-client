a51fc9a618ee19151914d1e120033290d50a7675
biupripro <biupripro@163.com>
2023-04-05 22:35:51 +0800

    实现通过sock代理下载pixiv的图片

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,25 +15,25 @@ fun main(args: Array<String>) {
     }.build()
     println("http client on socks proxy created")
 
+    val imgPath = "img-original/img/2023/04/05/07/00/17/106879191_p0.png"
+
     val request = Request.Builder().apply {
-        url("https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png")
-//        addHeader("Referrer-Policy", "strict-origin-when-cross-origin")
-//        addHeader(":authority", "i.pximg.net")
-//        addHeader(":method", "GET")
-//        addHeader(":path", "/img-original/img/2023/04/05/17/00/06/106888963_p0.png")
-//        addHeader(":scheme", "https")
+        url("https://i.pximg.net/${imgPath}")
+        addHeader("referer", "https://www.pixiv.net/")
     }.build()
     println("http request created [${request.headers}]")
 
     val resp = client.newCall(request).execute()
     println("http response got, code is [${resp.code}]")
 
-    val bytes = resp.body?.bytes() ?: ByteArray(0)
-    File("D:/tmp/zzz.png").apply {
-        parentFile.mkdirs()
-        writeBytes(bytes)
+    if (resp.code == 200) {
+        val bytes = resp.body?.bytes() ?: ByteArray(0)
+        File("D:/tmp/zzz.png").apply {
+            parentFile.mkdirs()
+            writeBytes(bytes)
+        }
     }
 
-    val n = 0
+    println("bye")
 
 }
