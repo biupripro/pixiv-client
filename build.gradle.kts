102435ee66bc4790b642f5f5bd7526115fd36745
biupripro <biupripro@163.com>
2023-04-05 22:00:37 +0800

    实现通过socks系统代理访问pixiv

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,27 @@
+import okhttp3.*
+import java.net.InetSocketAddress
+import java.net.Proxy
+import java.net.ProxySelector
+import java.net.URI
+import java.util.concurrent.TimeUnit
+
 fun main(args: Array<String>) {
     println("Hello, my Pixiv Client")
+
+    val client = OkHttpClient.Builder().apply {
+        proxy(Proxy(Proxy.Type.SOCKS, InetSocketAddress("127.0.0.1", 63474)))
+        readTimeout(5000, TimeUnit.MINUTES)
+    }.build()
+    println("http client on socks proxy created")
+
+    val request = Request.Builder().apply {
+        url("https://i.pximg.net/img-original/img/2023/04/05/17/00/06/106888963_p0.png")
+    }.build()
+    println("http request created")
+
+    val resp = client.newCall(request).execute()
+    println("http response got, code is [${resp.code}]")
+
+    val n = 0
+
 }
